033e6aef72b7e2da933e50e2ede000bba62ab6bc
kimia karbin <kimiakarbin@gmail.com>
2026-01-02 16:01:56 +0100

    Initial Commit

---
+++ MapExercise/MapExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapExerciseApp.swift
+//  MapExercise
+//
+//  Created by KimiaKarbin on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MapExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
