6c5230a81459e4741562080eab21b61f1dd9c5c7
kimia karbin <kimiakarbin@gmail.com>
2026-01-04 21:27:24 +0100

    all features added

---
+++ MapExercise/MapPolygons.swift
@@ -0,0 +1,12 @@
+//
+//  Untitled.swift
+//  MapExercise
+//
+//  Created by KimiaKarbin on 04/01/26.
+//
+import MapKit
+let turinPolygon: [CLLocationCoordinate2D] = [
+    CLLocationCoordinate2D(latitude: 45.07024, longitude: 7.51988),
+    CLLocationCoordinate2D(latitude: 45.00069, longitude: 7.69202),
+    CLLocationCoordinate2D(latitude: 45.02160, longitude: 7.58226)
+]
